5df6cc0343ee1d7305b08d20f2d9b0d9a763d7e3
MASKY-33 <masky.somebody33@gmail.com>
2026-06-06 21:32:08 +0200

    Create firstLastWord.py

---
+++ firstLastWord.py
@@ -0,0 +1,8 @@
+given_sentence = input("give any sentence: ")
+
+word_split = given_sentence.split() # See the, by me given variable names, as a hint to the meaning of the action. In this case the variable name; words_apart = split()
+
+first_word = word_split[0]
+last_word = word_split[-1]
+
+print(first_word, last_word)
